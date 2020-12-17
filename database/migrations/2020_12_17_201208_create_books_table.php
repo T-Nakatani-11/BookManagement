ca32954683b0a11c58689a0387ce59cfdc251e74
T-Nakatani-11 <aksrn.n11@gmail.com>
2020-12-18 00:12:38 +0900

    Create migration files

---
+++ database/migrations/2020_12_17_201208_create_books_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateBooksTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        if(!Schema::hasTable('books')){
+            Schema::create('books', function (Blueprint $table) {
+                $table->bigIncrements('id');
+                $table->String('title',100);
+                $table->text('content');
+                $table->timestamps();
+            });
+        }
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('books');
+    }
+}
