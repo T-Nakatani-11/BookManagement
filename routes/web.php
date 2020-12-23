53e8e7d0b15839daf07e81b6268bd09ae4e59211
T-Nakatani-11 <aksrn.n11@gmail.com>
2020-12-24 00:06:13 +0900

    Fix_Routes

--- routes/web.php
+++ routes/web.php
@@ -16,11 +16,11 @@ use Illuminate\Support\Facades\Route;
 // 書籍一覧表示
 Route::get('/', 'App\Http\Controllers\BookController@showList')->name('books');
 
-// 書籍詳細表示
-Route::get('/book/{id}', 'App\Http\Controllers\BookController@showDetail')->name('detail');
-
 // 書籍登録画面表示
 Route::get('/book/create', 'App\Http\Controllers\BookController@showCreate')->name('create');
 
 // 書籍登録
 Route::post('/book/store', 'App\Http\Controllers\BookController@exeStore')->name('store');
+
+// 書籍詳細表示
+Route::get('/book/{id}', 'App\Http\Controllers\BookController@showDetail')->name('detail');
