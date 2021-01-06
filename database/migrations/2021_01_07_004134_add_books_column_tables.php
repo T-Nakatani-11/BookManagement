156c72d7b5e7cab81f5901a566ecbe894fdd6c24
vagrant <vagrant@homestead>
2021-01-06 15:43:20 +0000

    CreateMigrationFile

--- database/migrations/2021_01_07_004134_add_books_column_tables.php
+++ database/migrations/2021_01_07_004134_add_books_column_tables.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class AddImageColumnBooksTable extends Migration
+class AddBooksColumnTables extends Migration
 {
     /**
      * Run the migrations.
@@ -14,8 +14,7 @@ class AddImageColumnBooksTable extends Migration
     public function up()
     {
         Schema::table('books', function (Blueprint $table) {
-            // 画像カラム追加
-            $table->binary('image');
+            //
         });
     }
 
@@ -27,8 +26,7 @@ class AddImageColumnBooksTable extends Migration
     public function down()
     {
         Schema::table('books', function (Blueprint $table) {
-            // 画像カラム削除
-            $table->dropColumn('image');
+            //
         });
     }
 }
