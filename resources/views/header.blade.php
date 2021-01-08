ecf6f5ae331aa3401e6483d51503dfe742eb7e9a
T-Nakatani-11 <aksrn.n11@gmail.com>
2021-01-09 01:39:52 +0900

    FixViewSide

--- resources/views/header.blade.php
+++ resources/views/header.blade.php
@@ -6,26 +6,19 @@
   <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
     <div class="navbar-nav">
       <a class="nav-item nav-link active" href="{{route('books')}}">書籍一覧 <span class="sr-only"></span></a>
-      <a class="nav-item nav-link" href="{{route('create')}}">書籍追加</a>
-      <div class="container">
-    	  <div class="row">
-          <div class="col-md-100">
-            <div id="custom-search-input">
-                 <div class="input-group col-md-100">
-                    <input type="text" class="form-control input-lg" placeholder="書籍名を入力" />
-                    <span class="input-group-btn">
-                        <button class="btn btn-info btn-lg" type="button">
-                            <i class="glyphicon glyphicon-search"></i>
-                        </button>
-                    </span>
-                </div>
-           </div>
-        </div>
+      <a class="nav-item nav-link" href="{{route('create')}}" >書籍追加</a>
+    </dev>
+    <div class="col-md-6">
+    <div id="custom-search-input">
+      <div class="input-group col-md-12">
+        <input type="text" class="form-control input-lg" placeholder="書類名を入力してください" size="100" style="height:3em"/>
+        <span class="input-group-btn">
+          <button class="btn btn-info btn-lg" type="button">
+            <i class="fas fa-search"></i>
+          </button>
+        </span>
       </div>
-	  </div>
+    </div>
+	</div>
 </div>
-
-
-
-  </div>
 </nav>
